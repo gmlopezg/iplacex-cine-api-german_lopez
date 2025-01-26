dd343276633cefa410766d29456f009069f63b3e
gmlopezg <166254766+gmlopezg@users.noreply.github.com>
2025-01-25 21:28:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
-import express from "express";
-import cors from "cors";
-import connectDB from "./src/common/db.js";  // Importa la función de conexión
-import peliculaRoutes from "./src/pelicula/routes.js";
-import actorRoutes from "./src/actor/routes.js";
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import peliculaRoutes from './src/pelicula/routes.js';
+import actorRoutes from './src/actor/routes.js';
+
+// Conexión a la base de datos MongoDB
+const uri = 'mongodb+srv://gmlopezg:uq7LQtQuQjk5OlRl@eva-u3-express.3fvdd.mongodb.net/?retryWrites=true&w=majority&appName=eva-u3-express';
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.status(200).send("Bienvenido al cine Iplacex 🚀"));
+// Rutas
+app.all('/', (req, res) => res.status(200).send('Bienvenido al cine Iplacex 🚀'));
+app.use('/cine-api/peliculas', peliculaRoutes);
+app.use('/cine-api/actores', actorRoutes);
 
-app.use("/cine-api/peliculas", peliculaRoutes);
-app.use("/cine-api/actores", actorRoutes);
-
-// Conexión a MongoDB y luego inicio del servidor
+// Conectar a la base de datos MongoDB
 const startServer = async () => {
-  await connectDB();  // Espera la conexión a la base de datos
+  try {
+    await client.connect();  // Conectar a MongoDB Atlas
+    console.log('✅ Conectado al clúster de MongoDB');
 
-  app.listen(PORT, () => {
-    console.log(`✅ Servidor corriendo en https://iplacex-cine-api-german-lopez.onrender.com`);
-  });
+    app.listen(PORT, () => {
+      console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ Error al conectar con MongoDB Atlas:', err);
+    process.exit(1);  // Detener el servidor si no se puede conectar
+  }
 };
 
 startServer(); 
